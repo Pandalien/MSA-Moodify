dc4b4360a7a1d6bea7e8ba46c871af18c562b7f4
Pandalien <lennychen@live.com>
2016-08-06 13:54:46 +1200

    initial release

---
+++ helloPerson.js
@@ -0,0 +1,5 @@
+function greeter(person) {
+    return "Hello, " + person;
+}
+var user = "Deep Sea Fish big";
+document.body.innerHTML = greeter(user);
---
+++ helloPerson.ts
@@ -0,0 +1,7 @@
+function greeter(person: string) {
+    return "Hello, " + person;
+}
+
+var user = "Deep Sea Fish big";
+
+document.body.innerHTML = greeter(user);
